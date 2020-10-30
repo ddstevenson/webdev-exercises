365a8f0f1196affade9e9c475c767b798ba0d4d7
ddstevenson <MY_NAME@example.com>
2020-10-30 15:08:36 -0700

    Finished problem 1

--- hw2/01-fizzbuzz.js
+++ hw2/01-fizzbuzz.js
@@ -1,1 +1,18 @@
 // Enter your code here
+fzbz(100);
+
+function fzbz(num) {
+    for(x=0; x<=num; ++x){
+        var output = "";
+        if(x % 3 == 0 && x % 5 == 0){
+            output = "fizzbuzz"
+        } else if(x % 3 == 0){
+            output = "fizz"
+        } else if (x%5 == 0){
+            output = "buzz"
+        } else {
+            output = x;
+        }
+        document.getElementById("results").innerHTML += (output + "<br>");
+    }
+  }
