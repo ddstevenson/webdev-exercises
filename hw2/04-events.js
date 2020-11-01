2bdaa8094959d097faaa0bb9aca9473fb1ed1bb3
ddstevenson <MY_NAME@example.com>
2020-11-01 14:12:48 -0800

    finished problem 4

--- hw2/04-events.js
+++ hw2/04-events.js
@@ -1,1 +1,7 @@
 // Enter your code here
+window.addEventListener('resize', printwh);
+printwh();
+function printwh(){
+    document.getElementById("width").innerHTML = window.innerWidth;
+    document.getElementById("height").innerHTML = window.innerHeight;
+}
