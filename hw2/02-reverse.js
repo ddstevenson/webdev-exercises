46da0e266b1b8bed04a8eeb67947bfaeb09925c8
ddstevenson <MY_NAME@example.com>
2020-11-01 11:38:23 -0800

    Finished Problem 2

--- hw2/02-reverse.js
+++ hw2/02-reverse.js
@@ -1,1 +1,14 @@
 // Enter your code here
+function validateForm(){
+    var input = document.getElementById("input").value.trim();
+    if(input.length != 8){
+        document.getElementById("msg").innerHTML = "You must enter exactly eight (8) digits.";
+        return false;
+    } else if(isNaN(input)){ // This case shouldn't come up often as the form is numeric-only.
+        document.getElementById("msg").innerHTML = "You must enter eight NUMERIC characters.";
+        return false;
+    } else { // input is valid
+        document.getElementById("output").innerHTML = input.split("").reverse().join("");
+        return false;
+    }
+}
