804f8ca9df21fef91298167ef00e0d15a9123e81
ddstevenson <MY_NAME@example.com>
2020-11-01 14:52:15 -0800

    Finished Problem 5

--- hw2/05-api.js
+++ hw2/05-api.js
@@ -1,3 +1,14 @@
 const url = 'https://restcountries.eu/rest/v2/all';
 
 // Enter your code here
+var request = new XMLHttpRequest();
+request.open('GET', url, true)
+request.onload = function () {
+    var data = JSON.parse(this.response);
+    if (request.status >= 200 && request.status < 400) {
+        data.forEach((country) => {
+        document.getElementById("results").innerHTML += `<li>${country.name} - ${country.population.toLocaleString()}</li>`;
+        })
+    }
+}
+request.send();
