7b423b317854de8a1f0e9778044e6dd3fa7d5754
ddstevenson <MY_NAME@example.com>
2020-11-15 13:50:34 -0800

    p2 finished

--- hw3/02-node-url.js
+++ hw3/02-node-url.js
@@ -5,11 +5,20 @@ const querystring = require('querystring');
 const port = process.env.PORT || 5000;
 
 // Add your code below
+url.querystring
 
 const server = http.createServer((req, res) => {
     
     // Add your code below
-
+    const qs = req.url.split("?")[1];
+    var queryData = querystring.parse(qs ? qs : '');
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<html><body><table>");
+    for(var x in queryData) {
+      console.log(queryData);
+      res.write(`<tr><td>${x}</td><td>${queryData[x]}</td></tr>`);
+    }
+    res.write("</table></body></html>");
     res.end();
   });
   
