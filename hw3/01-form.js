0eec6c1c274641caceae1d2aac5624380e1d4bbd
ddstevenson <MY_NAME@example.com>
2020-11-15 12:30:02 -0800

    Finished p11

--- hw3/01-form.js
+++ hw3/01-form.js
@@ -1,1 +1,11 @@
 // Enter your code here
+// No idea what this file is for -- so storing links here
+<html>
+<body>
+<p><a href="http://localhost:5000/">http://localhost:5000/</a></p>
+<p><a href="http://localhost:5000/redirect">hhttp://localhost:5000/redirect</a></p>
+<p><a href="http://localhost:5000/cache">http://localhost:5000/cache</a></p>
+<p><a href="http://localhost:5000/cookie">http://localhost:5000/cookie</a></p>
+<p><a href="http://localhost:5000/check-cookies">http://localhost:5000/check-cookies</a></p>
+</body>
+</html>
--- hw3/01-node-routes.js
+++ hw3/01-node-routes.js
@@ -1,24 +1,46 @@
 const http = require('http'); 
 const port = process.env.PORT || 5000;
-
-// http://localhost:5000/ should return a status code 200 with a 'welcome' message
-
-// http://localhost:5000/redirect should redirect the request to '/redirected' by using 302 as the status code
-
-// http://localhost:5000/cache should return 'this resource was cached' in plain text and set the cache max age to a day
-
-// http://localhost:5000/cookie should return 'cookies… yummm' in plain text and set 'hello=world' as a cookie
-
-// http://localhost:5000/check-cookies should return 'yes' / 'no' in plain text depending on whether the browser has the 'hello' cookie
-
-// for other routes, this exercise should return a status code 404 with '404 - page not found' in plain text
+var cookie = require('cookie');
 
 const server = http.createServer((req, res) => {
-    
-    // Add your code below
+  var cookies = cookie.parse(req.headers.cookie || '');
 
-    res.end();
-  });
+  // http://localhost:5000/ should return a status code 200 with a 'welcome' message
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>Welcome to this application!</h1>");
+  }
+  // http://localhost:5000/redirect should redirect the request to '/redirected' by using 302 as the status code
+  else if (req.url === "/redirect") {
+    res.writeHead(302, { "Location": "/redirected" });
+  }
+  // http://localhost:5000/cache should return 'this resource was cached' in plain text and set the cache max age to a day
+  else if (req.url === "/cache") {
+    res.writeHead(200, { "Content-Type": "text/plain", 'Cache-Control':'max-age=86400' });
+    res.write("this resource was cached");
+  }
+  // http://localhost:5000/cookie should return 'cookies… yummm' in plain text and set 'hello=world' as a cookie
+  else if (req.url === "/cookie") {
+    res.writeHead(200, { "Content-Type": "text/plain", 'Set-Cookie':'hello=world' });
+    res.write("cookies. . . yummm");
+  }
+  // http://localhost:5000/check-cookies should return 'yes' / 'no' in plain text depending on whether the browser has the 'hello' cookie
+  else if (req.url === "/check-cookies") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    if(cookies.hello){
+      res.write("yes");
+    } else {
+      res.write("no");
+    }
+  }
+  // for other routes, this exercise should return a status code 404 with '404 - page not found' in plain text
+  else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.write("404 - page not found");
+  }
+
+  res.end();
+});
   
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
