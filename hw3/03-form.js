9cdd8e741ae449f0f728d536495cadc34a81f522
ddstevenson <MY_NAME@example.com>
2020-11-15 14:58:04 -0800

    P3 finished

--- hw3/03-form.js
+++ hw3/03-form.js
@@ -5,9 +5,19 @@ const port = process.env.PORT || 5000;
 // Add your code below
 
 app.use(express.static('files'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+  }));
 
-app.post('/', (req, res) => {
+app.post('/submit', (req, res) => {
     // Add your code below
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write(`Name: ${req.body.name}
+Email: ${req.body.email}
+Message: ${req.body.message ? req.body.message : "n/a"}
+Newsletter: ${req.body.checkbox1 ? "yes" : "no"}`);   
+    res.end();
 });
     
 app.listen(port, () => {
