316b09feefe85d5b7c4968c65f7bdbe4abbe455d
ddstevenson <MY_NAME@example.com>
2020-11-15 17:49:27 -0800

    Finished p4

--- hw3/04-sessions.js
+++ hw3/04-sessions.js
@@ -5,12 +5,51 @@ const port = process.env.PORT || 5000;
 
 // Add your code below
 
-app.use(/** ... */);
+app.use(session({secret: '28828758144247498801',saveUninitialized: true, resave:true}));
 
 app.get('/', (req, res) => {
     // Add your code below
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write(`${current_route(req)}`);
+    res.write("\n\n");
+    if(!req.session.history){ // Only display if new session AND requested root
+        res.write("Welcome to http://localhost:5000/");
+        req.session.history = ["/"]
+    }else {
+        res.write(route_history(req));
+        req.session.history.push("/");
+    }
+    res.end();
+});
+
+app.get('/*', (req, res) => {
+    // Add your code below
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write(`${current_route(req)}`);
+    res.write("\n\n");
+    if(!req.session.history){
+        req.session.history = [req.path];
+    }else {
+        req.session.history.push(req.path);
+    }
+    res.write(route_history(req));
+    res.end();
 });
     
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
+function current_route(req){
+    return `Currently on route: ${req.path}`;
+}
+
+function route_history(req){
+    var retval = `Previously visited:
+`;
+    req.session.history.forEach( (x) => {
+        retval += `${x}
+`;
+    });
+    return retval;
+}
