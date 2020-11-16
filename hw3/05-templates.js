10338eec6a5e45be91aaf96ebe0c4787e0b7bc8b
ddstevenson <MY_NAME@example.com>
2020-11-15 20:56:08 -0800

    Finished exercise 5

--- hw3/05-templates.js
+++ hw3/05-templates.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const app = express();
+const request = require('request');
 const port = process.env.PORT || 5000;
+const surl = 'https://restcountries.eu/rest/v2/all';
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
 // Add your code below
 
-app.get('/', (req, res) => {
-   
+app.get('/main', (req, res) => {
+    request(surl, { json: true }, (err, resp, body) => {
+        if (err) { return console.log(err); }
+        res.render('index', { title: 'Express', data: body });
+    });
+});
+
+app.get('/populous', (req, res) => {
+    request(surl, { json: true }, (err, resp, body) => {
+        if (err) { return console.log(err); }
+        res.render('pop', { title: 'Express', data: body });
+    });
+});
+
+app.get('/regions', (req, res) => {
+    request(surl, { json: true }, (err, resp, body) => {
+        if (err) { return console.log(err); }
+        res.render('reg', { title: 'Express', data: body });
+    });
 });
 
 app.listen(port, () => {
